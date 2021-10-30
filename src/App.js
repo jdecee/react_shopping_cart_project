6dd8db72090e8af3fed0c59b26604928471583bf
jdecee <jdecee@gmail.com>
2021-10-29 23:13:35 -0400

    setState to App Level.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react'
 import Button from './components/Button'
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count : 0
+    }
+  }
+
+  // **The setState method (which is a built-in react method) forces the render function the render again**
+
+  handleclick = (step) => {
+    let newCount = this.state.count + step
+    this.setState({
+        count: newCount
+    })
+}
+
+
   render() {
+    const pep_talk = "UNDER ANY CIRCUMSTANCES DON'T GIVE UP!"
     return (
-      <div>
-        <h1>Hello World</h1>
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
+        <div className='container'>
+        <h1>{this.props.name}, {pep_talk}</h1>
+        <Button step={1} incrementCount={this.handleclick} />
+        <Button step={5} incrementCount={this.handleclick} />
+        <Button step={10} incrementCount={this.handleclick} />
+        <Button step={25} incrementCount={this.handleclick} />
+        <Button step={100} incrementCount={this.handleclick} />
+        <h6>Count is at {this.state.count}</h6>
+        {/* Now that the state is at the App.js level, the count can be totaled for all the buttons not just one button individually. */}
       </div>
     )
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
+// When the page is refreshed the component is constructed again? 
+
 export default class Button extends Component {
     render() {
         return (
-            <div>
-                <button>Click Me!</button>
+// 1. He said something about references which is why we switched the onClick to an arrow function.
+// 2. this.props.incrementCount is the property on the button (set within the green component buttons in app.js).
+// 3. incrementCount(this.props.step) refers to the step property number for each button (set inside the green jsx buttons in app.js). 
+//    that gets passed into the handleclick function because incrementCount=handleclick...(this.props.step) is the step paramater in handleclick(step).
+// 4. +{this.props.step} is just for visual purposes on the button, also refers to the step property number for each button.
+                                                    
+            <div>                                   
+                <button className='btn btn-primary' onClick={() => this.props.incrementCount(this.props.step)}>+{this.props.step}</button>
             </div>
         )
     }
